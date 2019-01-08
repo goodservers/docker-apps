5cf33955ff27bd123598e440654794322e3febfc
Tomas Wagner <tomas.wagner@gmail.com>
2019-01-08 16:39:27 +0100

    🧹 clean

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -42,10 +42,6 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
 	if [ ! -e index.php ] && [ ! -e wp-includes/version.php ]; then
 		echo >&2 "WordPress not found in $PWD - copying now..."
-		# if [ "$(ls -A)" ]; then
-		# 	echo >&2 "WARNING: $PWD is not empty - press Ctrl+C now if this is an error!"
-		# 	( set -x; ls -A; sleep 10 )
-		# fi
 		tar --create \
 			--file - \
 			--one-file-system \
